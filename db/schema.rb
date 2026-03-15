d8dada9c483802b10b36bf9600760459349463d4
charahana <charatwoha@outlook.jp>
2026-03-15 22:58:47 +0000

    feat: Answerマイグレーションを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_03_14_131758) do
+ActiveRecord::Schema.define(version: 2026_03_15_225555) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "question_id", null: false
+    t.integer "selected_answer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text "question_text"
@@ -37,4 +47,6 @@ ActiveRecord::Schema.define(version: 2026_03_14_131758) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
 end
