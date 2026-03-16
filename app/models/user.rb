3f22df014f437ddce6922e1a5904ae6a8ef6f85a
charahana <charatwoha@outlook.jp>
2026-03-16 02:11:40 +0000

    feat: usersとの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :answers, dependent: :destory
+
   validates :name, presence: true, length: { maximum: 50 }
 end
