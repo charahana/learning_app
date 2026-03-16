ad3c82d44e48991db0ae3f5d62608cb35aa64796
charahana <charatwoha@outlook.jp>
2026-03-16 06:39:43 +0000

    feat: 選択肢及び解説のバリデーションを実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,10 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+
+  valitates: question_text, presence: true
+  valitates: choice1, presence: true
+  valitates: choice2, presence: true
+  valitates: choice3, presence: true
+  valitates: choice4, presence: true
+  valitates: correct_answer, presence: true
 end
