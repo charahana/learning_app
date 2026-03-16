f296a819c6a66d481b09d173ae042d367dc1ff67
charahana <charatwoha@outlook.jp>
2026-03-16 02:16:34 +0000

    feat: Answersコントローラを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :answers, dependent: :destory
+  has_many :answers, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
 end
