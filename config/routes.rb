ceede32cbc4082c00ff406a19bbcf9f4c63008f4
charahana <charatwoha@outlook.jp>
2026-03-17 05:31:20 +0000

    feat: ルーティングに回答履歴を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :questions do
-    resources :answers, only: [:create]
+    resources :answers, only: [:create, :index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
