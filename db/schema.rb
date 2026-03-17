f81eaa5aa85b064aedfb3f777e89b16cc1a41a65
charahana <charatwoha@outlook.jp>
2026-03-17 01:52:05 +0000

    Categoriesマイグレーションを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_03_15_225555) do
+ActiveRecord::Schema.define(version: 2026_03_17_014653) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2026_03_15_225555) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
