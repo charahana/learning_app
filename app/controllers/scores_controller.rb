39e9697f456293a32fdfd3708a42de003b77c159
charahana <charatwoha@outlook.jp>
2026-03-18 12:04:39 +0000

    feat:scoreコントローラにindexを定義

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,4 +1,13 @@
 class ScoresController < ApplicationController
   def index
+    answers = current_user.answers.joins(:question)
+    @total = ansers.count
+    @corrent = ansers.where("selected_answer = questions.correct_answer").count
+    @accuracy =
+      if @total > 0
+        (@correct.to_f / @total * 100.round(1))
+      else
+        0
+      end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :answers, only: [:create, :index]
   end
 
-  resources :score, only: [:show]
+  resources :score, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/about', to: 'homes#about', as: 'about'
