f8b6f2279a8ce58069cbd97c253a9c72062056b5
charahana <charatwoha@outlook.jp>
2026-04-06 23:46:04 +0000

    refactor: コードの簡略化

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,12 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
@@ -24,12 +24,10 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find(params[:id])
     (4 - @question.choices.size).times { @question.choices.build }
   end
 
   def update
-    @question = Question.find(params[:id])
     if @question.update(question_params)
       redirect_to @question
     else
@@ -38,7 +36,6 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
     redirect_to questions_path
   end
@@ -47,5 +44,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:question_text, :explanation)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
