2bd06cb60b509b02b2e8c6d3146e745443627fe6
charahana <charatwoha@outlook.jp>
2026-04-11 12:20:01 +0000

    feat: ルーティングにrandomを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :questions do
     resources :answers, only: [:create]
+    collection do
+      get :random
+    end
   end
   
   resources :answers, only: [:index]
