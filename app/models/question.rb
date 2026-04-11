ddebca791f076ba0d26f09a22d59fea9781777d6
charahana <charatwoha@outlook.jp>
2026-04-11 00:23:27 +0000

    feat: 正解取得を効率化

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,8 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :choices, allow_destroy: true
 
   validates :question_text, presence: true
+
+  def correct_choice
+    choice.find_by(is_correct: true)
+  end
 end
