c7e0e04aaaea2a6961f4add67882affffdf572fe
charahana <charatwoha@outlook.jp>
2026-04-13 13:11:34 +0000

    refactor: categoryモデルのRspec内容を修正

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,11 +1,16 @@
+require 'rails_helper'
+
 RSpec.describe Category, type: :model do
-    it 'nameがあれば有効' do
-      category = Category.new(name: "テスト")
-      expect(category).to be_valid
-    end
-  
-    it 'nameがなければ無効' do
-      category = Category.new(name: nil)
+  describe 'バリデーション' do
+    it { should validate_presence_of(:name) }
+
+    it 'nameが空文字なら無効' do
+      category = Category.new(name: "")
       expect(category).to be_invalid
     end
   end
+
+  describe 'アソシエーション' do
+    it { should have_many(:questions) }
+  end
+end
