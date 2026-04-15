36f3d472daa81551c6500c2d68aae7acd064631d
charahana <charatwoha@outlook.jp>
2026-04-15 13:11:41 +0000

    feat: homeコントローラのtopを定義

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,12 @@
 class HomesController < ApplicationController
 
   def top
+    return unless user_signed_in?
+    answers = current_user.answers.includes(question: :category)
+    @total = answers.count
+    @correct = answers.count(&:correct?)
+    @accuracy = @total.zero? ? 0 : (@correct.to_f / @total * 100).round
+    @weak_categories = []
   end
 
   def about
