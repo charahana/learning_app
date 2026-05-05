c5fe5866290b41f347f080c3e46cdddcdc90dd86
charahana <charatwoha@outlook.jp>
2026-05-05 22:52:30 +0000

    feat: 管理者用ユーザー機能を実装

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,55 @@
+class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "ユーザーを作成しました"
+    else
+      flash.now[:alert] = "ユーザー作成に失敗しました"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user == current_user
+      redirect_to admin_users_path, alert: "自分自身は削除出来ません"
+    else
+      @user.destroy
+      redirect_to admin_users_path, notice: "削除しました"
+    end
+  end
+
+  private
+
+  def user_params
+    parmitted = [:email, :name, :admin]
+    if params[:user][:password].present?
+      permitted += [:password, :password_confirmation]
+    end
+    params.require(:user).permit(permitted)
+  end
+end
